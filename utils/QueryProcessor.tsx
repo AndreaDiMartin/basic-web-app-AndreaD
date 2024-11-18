01a42aa133385a085cf9411715a7d082930fd523
AndreaDiMartin <18-10826@usb.ve>
2024-11-18 14:15:48 -0400

    Añade query para saber el nombre

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -7,6 +7,12 @@ export default function QueryProcessor(query: string): string {
     );
   }
 
+  if(query.toLowerCase().includes("your name")) {
+    return (
+      "Andrea"
+    );
+  }
+
   if (query.toLowerCase().includes("usb id")) {
     // TODO añade tu USB ID a continuación
     // TODO actualiza el caso de prueba correspondiente en __tests__
